50a5bbe1e06cf63fca5bd23c4a8c08fc35351eb0
KelvinLamHK <kelvinlamtatyin@gmail.com>
2023-02-28 15:09:33 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "../src/pages/LoginPage.jsx"
 import LandingPage from "../src/pages/LandingPage.jsx"
+import CampaignPage from "../src/pages/CampaignPage.jsx"
+import DocCenterPage from "../src/pages/DocCenterPage.jsx"
+import BrokerComPage from "../src/pages/CMS/BrokerComPage.jsx"
+import CategoriesPage from "../src/pages/CMS/CategoriesPage.jsx"
+import EventCalendarPage from "../src/pages/CMS/EventCalendarPage.jsx"
+import QuickLinksPage from "../src/pages/CMS/QuickLinksPage.jsx"
+
+
 
 function App() {
   return (
@@ -10,6 +18,12 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/home" element={<LandingPage />} />
         <Route path="/Landing" element={<LandingPage />} />
+        <Route path="/Campaign" element={<CampaignPage />} />
+        <Route path="/DocCenter" element={<DocCenterPage />} />
+        <Route path="/BrokerCom" element={<BrokerComPage />} />
+        <Route path="/Categories" element={<CategoriesPage />} />
+        <Route path="/EventCalendar" element={<EventCalendarPage />} />
+        <Route path="/QuickLinks" element={<QuickLinksPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/CMS/EventCalendarPage.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import PlusNavbar from "../../components/PlusNavbar";
+import LoadingScreen from "../../components/LoadingScreen";
+import "bootstrap/dist/css/bootstrap.css";
+import { getCurrentBrowserFingerPrint } from "@rajesh896/broprint.js";
+
+
+
+function EventCalendarPage() {
+  const [username, setUsername] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const token = Cookies.get("PLUSID");
+
+  useEffect(() => {
+    async function fetchData() {
+      const deviceId = await getCurrentBrowserFingerPrint();
+      if (!token) {
+        window.location.href = "/login";
+      } else {
+        try {
+          const response = await fetch("http://kayu.life:8081/protected", {
+            method: "POST",
+            headers: {
+              Authorization: 'plus ' + token,
+              DeviceId: deviceId,
+            },
+          });
+  
+          const data = await response.text();
+          if (data === "Invalid") { // check if response is "Invalid"
+            window.location.href = "/login";
+          } else {
+            const jsonData = JSON.parse(data); // parse response as JSON
+            setUsername(jsonData.username);
+            setIsLoading(false);
+          }
+        } catch (error) {
+          console.error(error);
+          setIsLoading(false);
+        }
+      }
+    }
+  
+    fetchData();
+  }, [token]);
+
+  return (
+    <>
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <>
+        <PlusNavbar />
+        <h1>{username}</h1>
+        </>
+      )}
+    </>
+  );
+}
+
+export default EventCalendarPage;
